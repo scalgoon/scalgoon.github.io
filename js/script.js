872701b2e6f7b4d14bfb09b83765cbb372f64b4c
Dez P <63563412+scalgoon@users.noreply.github.com>
2022-03-19 16:55:30 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,4 @@
-if(document.URL.includes('goose.thedev.id')) {
 
-  const ThemeToUse = localStorage.getItem("SiteTheme");
-
-if(ThemeToUse == 1) {
-  document.body.classList.add("darkTheme"); 
-} else if (ThemeToUse == 2) {
-  document.body.classList.remove("darkTheme"); 
-}
-
-  //THEME
-var sun = document.getElementById('sun');
-
-var moon = document.getElementById('moon');
-
-sun.addEventListener('click', () => {
- document.body.classList.remove("darkTheme");
-//  console.log("SUN")
-
-localStorage.setItem("SiteTheme", 2);
-}) 
-
-moon.addEventListener('click', () => {
-  document.body.classList.add("darkTheme");  
-  // console.log("MOON")
-
-  localStorage.setItem("SiteTheme", 1);
-})
-
-
-//THEME
-}
-
-
-
-if(document.URL.includes('goose.thedev.id/goot')) {
 // MODAL FUNCTIONS
 
 var modal = document.getElementById("myModal");
@@ -58,10 +23,11 @@ window.onclick = function(event) {
 
 const ThemeToUse = localStorage.getItem("SiteTheme");
 
+console.log(ThemeToUse)
+
 if(ThemeToUse == 1) {
   document.body.classList.add("darkTheme"); 
 } else if (ThemeToUse == 2) {
   document.body.classList.remove("darkTheme"); 
 }
 
-}
