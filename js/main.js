6cfd8e6d1066e302dc7ba08de4a44cad498684b2
Dez P <63563412+scalgoon@users.noreply.github.com>
2022-11-12 12:26:02 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
-let min = 0, max = 359;
+const ThemeToUse = localStorage.getItem("SiteTheme");
 
-let randomHue = Math.round(Math.random() * (max - min) + min);
+if(ThemeToUse == 1) {
 
-let cssRoot = document.documentElement;
+    let cssRoot = document.documentElement;
 
-cssRoot.style.setProperty('--darkColor', `hsl(${randomHue}, 72%, 8%)`);
+    cssRoot.style.setProperty('--siteBackgroundPc', "none");
+    
+    cssRoot.style.setProperty('--siteBackgroundMobile', "none");
 
-cssRoot.style.setProperty('--lightColor', `hsl(${randomHue}, 72%, 68%)`);
+} else if (ThemeToUse == 2) {
+
+    let min = 0, max = 359;
+
+    let randomHue = Math.round(Math.random() * (max - min) + min);
+    
+    let cssRoot = document.documentElement;
+    
+    cssRoot.style.setProperty('--darkColor', `hsl(${randomHue}, 72%, 8%)`);
+    
+    cssRoot.style.setProperty('--lightColor', `hsl(${randomHue}, 72%, 68%)`);
+
+} else if (!ThemeToUse) {
+
+    let min = 0, max = 359;
+
+    let randomHue = Math.round(Math.random() * (max - min) + min);
+    
+    let cssRoot = document.documentElement;
+    
+    cssRoot.style.setProperty('--darkColor', `hsl(${randomHue}, 72%, 8%)`);
+    
+    cssRoot.style.setProperty('--lightColor', `hsl(${randomHue}, 72%, 68%)`);
+
+}
